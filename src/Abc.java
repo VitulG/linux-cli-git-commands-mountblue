90324bca3adcfa88694099ad7160b54ebfb84f15
Vitul Gupta <guptavitul@gmail.com>
2025-11-08 11:00:17 +0530

    updated the changes in Abc.java

---
+++ src/Abc.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Abc {
+	private int x;
+
+    	public Abc(int x) {
+      		this.x = x;
+    	}
+
+	public static void m() {
+		System.out.println("x="+x);
+	}
+}
